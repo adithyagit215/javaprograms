5c35dd7f1036849ec948c0f8508ee4c85da600f9
adithyagit215 <adithyaspotify7@gmail.com>
2025-12-30 22:29:31 +0530

    Create multiplicationOfThree.java

---
+++ multiplicationOfThree.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+
+class multiplicationOfThree {
+  public static void main(String args[]){
+      int a = 3;
+      for(int i=1;i<=10;i++){
+        System.out.println(a*i);
+      }
+   }
+}  
+  
